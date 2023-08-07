1085b00375d253331fc9226da315438dc175d772
timcastillogill <timgillviola@gmail.com>
2023-08-07 10:55:50 +0100

    Complete productHeader with required information

--- client/components/product/productHeader.jsx
+++ client/components/product/productHeader.jsx
@@ -5,6 +5,9 @@ const ProductHeader = ({ product }) => {
     <div>
       <h1>{product.name}</h1>
       <img src={product.img_url} alt="" />
+      <h3>
+        {product.power} // Packet of {product.quantity}
+      </h3>
     </div>
   );
 };
