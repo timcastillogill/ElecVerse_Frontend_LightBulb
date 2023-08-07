5df8c19c89ffdcf1e585e35798d05149b3d9eeed
timcastillogill <timgillviola@gmail.com>
2023-08-07 18:35:00 +0100

    Add styling for finePrint and convert section to footer

--- client/components/finePrint.jsx
+++ client/components/finePrint.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const FinePrint = () => {
   return (
-    <section>
-      <p>
+    <footer className="p-4 bg-hemocyanin">
+      <p className="text-xs font-thin text-slate-400">
         <small>
           Octopus Energy Ltd. is a company registered in England and Wales.
           Registered number: 09263424. Registered office: 33 Holborn, London,
           EC1N 2HT. Trading office: 20-24 Broadwick Street, London, W1F 8HT
         </small>
       </p>
-    </section>
+    </footer>
   );
 };
 
