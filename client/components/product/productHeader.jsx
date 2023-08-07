ad79ff5f29c20ea8eee8558bb1818be7df588e9b
timcastillogill <timgillviola@gmail.com>
2023-08-07 16:59:32 +0100

    Add tailwind styling to productHeader

--- client/components/product/productHeader.jsx
+++ client/components/product/productHeader.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const ProductHeader = ({ product }) => {
   return (
-    <div>
-      <h1>{product.name}</h1>
-      <img src={product.img_url} alt="" />
-      <h3>
+    <div className="m-4">
+      <img className="rounded-3xl" src={product.img_url} alt={product.name} />
+      <h1 className="text-3xl">{product.name}</h1>
+      <h3 className="mt-1 text-sm text-slate-400">
         {product.power} // Packet of {product.quantity}
       </h3>
     </div>
