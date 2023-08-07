d29ba8ce1f0142dc1a421379142754ba41adca53
timcastillogill <timgillviola@gmail.com>
2023-08-07 15:13:24 +0100

    Update Product component to render addToBasket component

--- client/pages/product.jsx
+++ client/pages/product.jsx
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 import Header from "../components/header";
 import ProductHeader from "../components/product/productHeader";
 import { products } from "../../server/db";
+import AddToCart from "../components/product/cart/addToCart";
 
 export default function Product() {
   const [productData, setProductData] = useState(products);
+  const [cartQuantity, setCartQuantity] = useState(1);
   return (
     <div>
       <Header />
       <ProductHeader product={productData[0]} />
+      <AddToCart product={productData[0]} quantity={cartQuantity} />
     </div>
   );
 }
