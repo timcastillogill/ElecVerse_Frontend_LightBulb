975cb582f1c86dace4630bceb7ae9b821f7ee71a
timcastillogill <timgillviola@gmail.com>
2023-08-07 17:37:07 +0100

    use small semenatic html for fine print

--- client/components/finePrint.jsx
+++ client/components/finePrint.jsx
@@ -4,9 +4,11 @@ const FinePrint = () => {
   return (
     <section>
       <p>
-        Octopus Energy Ltd. is a company registered in England and Wales.
-        Registered number: 09263424. Registered office: 33 Holborn, London, EC1N
-        2HT. Trading office: 20-24 Broadwick Street, London, W1F 8HT
+        <small>
+          Octopus Energy Ltd. is a company registered in England and Wales.
+          Registered number: 09263424. Registered office: 33 Holborn, London,
+          EC1N 2HT. Trading office: 20-24 Broadwick Street, London, W1F 8HT
+        </small>
       </p>
     </section>
   );
