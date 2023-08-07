f19ceaee5d8534286ab3720e796f7565f09f9602
timcastillogill <timgillviola@gmail.com>
2023-08-07 18:29:53 +0100

    Remove whitespace

--- client/components/productDescription.jsx
+++ client/components/productDescription.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ProductDescription = ({ productDescription }) => {
   return (
-    <section className="p-2 bg-hemocyanin ">
+    <section className="p-2 bg-hemocyanin">
       <div className="mx-2">
         <h3 className="mb-3 text-xl">Description</h3>
         <p className="text-base font-thin">{productDescription}</p>
