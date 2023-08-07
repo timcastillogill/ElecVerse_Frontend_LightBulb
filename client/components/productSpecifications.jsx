5b9d725618d69cf667a5c5282e85b608c6891270
timcastillogill <timgillviola@gmail.com>
2023-08-07 16:44:37 +0100

    Update with a tbody tag

--- client/components/productSpecifications.jsx
+++ client/components/productSpecifications.jsx
@@ -4,28 +4,30 @@ const ProductSpecifications = ({ product }) => {
   return (
     <section>
       <table>
-        <tr>
-          <td>Brand</td>
-          <td>{product.brand}</td>
-        </tr>
-        <tr>
-          <td>Item weight (g)</td>
-          <td>{product.weight}</td>
-        </tr>
-        <tr>
-          <td>Dimensions (cm)</td>
-          <td>
-            {product.height} x {product.width} x {product.length}
-          </td>
-        </tr>
-        <tr>
-          <td>Item Model number</td>
-          <td>{product.model_code}</td>
-        </tr>
-        <tr>
-          <td>Colour</td>
-          <td>{product.colour}</td>
-        </tr>
+        <tbody>
+          <tr>
+            <td>Brand</td>
+            <td>{product.brand}</td>
+          </tr>
+          <tr>
+            <td>Item weight (g)</td>
+            <td>{product.weight}</td>
+          </tr>
+          <tr>
+            <td>Dimensions (cm)</td>
+            <td>
+              {product.height} x {product.width} x {product.length}
+            </td>
+          </tr>
+          <tr>
+            <td>Item Model number</td>
+            <td>{product.model_code}</td>
+          </tr>
+          <tr>
+            <td>Colour</td>
+            <td>{product.colour}</td>
+          </tr>
+        </tbody>
       </table>
     </section>
   );
