3bf908b08e45e07b80ea012519d03b6b01e1a087
timcastillogill <timgillviola@gmail.com>
2023-08-07 16:51:45 +0100

    Add tailwind styling

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="flex justify-between">
-      <h1>octopusenergy</h1>
+    <div className="flex justify-between p-2 py-1 align-middle">
+      <h1 className="ml-1 text-xl">
+        <span className="mr-0.5">octopus</span>
+        <span className="antialiased font-thin">energy</span>
+      </h1>
 
       <img
-        className="h-4 w-4 flex-no-shrink fill-current"
+        className="py-2 pr-2 fill-current h-9"
         src="/basket.svg"
         alt="Your Shopping Basket"
       />
