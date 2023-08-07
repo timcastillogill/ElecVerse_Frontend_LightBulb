e0be12f1f72783545a113bd138759dfd38648445
timcastillogill <timgillviola@gmail.com>
2023-08-07 17:01:35 +0100

    Update margin on heading

--- client/components/product/productHeader.jsx
+++ client/components/product/productHeader.jsx
@@ -4,7 +4,7 @@ const ProductHeader = ({ product }) => {
   return (
     <div className="m-4">
       <img className="rounded-3xl" src={product.img_url} alt={product.name} />
-      <h1 className="text-3xl">{product.name}</h1>
+      <h1 className="mt-4 text-3xl">{product.name}</h1>
       <h3 className="mt-1 text-sm text-slate-400">
         {product.power} // Packet of {product.quantity}
       </h3>
