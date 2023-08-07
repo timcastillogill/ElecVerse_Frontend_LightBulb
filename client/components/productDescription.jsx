d3e2771aa20229cd2e375a8696a7471d9f83a53c
timcastillogill <timgillviola@gmail.com>
2023-08-07 18:20:47 +0100

    Add styling to Description Component

--- client/components/productDescription.jsx
+++ client/components/productDescription.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const ProductDescription = ({ productDescription }) => {
   return (
-    <section>
-      <h3>Description</h3>
-      <p>{productDescription}</p>
+    <section className="p-2 bg-hemocyanin ">
+      <div className="mx-2">
+        <h3 className="mb-3 text-xl">Description</h3>
+        <p className="text-base font-thin">{productDescription}</p>
+      </div>
     </section>
   );
 };
