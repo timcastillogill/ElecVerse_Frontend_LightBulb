7007d64f439aa8948e4a7eeea1fe3a5d35df9555
timcastillogill <timgillviola@gmail.com>
2023-08-08 08:58:20 +0100

    Add prop to send basket total to header

--- client/pages/product.jsx
+++ client/pages/product.jsx
@@ -6,10 +6,11 @@ import AddToCart from "../components/product/addToCart/addToCart";
 import ProductDescription from "../components/productDescription";
 import ProductSpecifications from "../components/productSpecifications";
 import FinePrint from "../components/finePrint";
+import Cart from "../components/cart/cart";
 
 export default function Product() {
   const [productData, setProductData] = useState(products);
-  const [cartQuantity, setCartQuantity] = useState(1);
+  const [cartQuantity, setCartQuantity] = useState(0);
 
   const handleQuantityChange = (newQuantity) => {
     setCartQuantity(newQuantity);
@@ -17,7 +18,7 @@ export default function Product() {
 
   return (
     <div>
-      <Header />
+      <Header basketTotal={cartQuantity} />
       <ProductHeader product={productData[0]} />
       <AddToCart
         productPrice={productData[0].price}
@@ -27,6 +28,7 @@ export default function Product() {
       <ProductDescription productDescription={productData[0].description} />
       <ProductSpecifications product={productData[0]} />
       <FinePrint />
+      <Cart productName={productData[0].name} productQuantity={cartQuantity} />
     </div>
   );
 }
