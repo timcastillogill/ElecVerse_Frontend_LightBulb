0653a305b124b2f7f0668b94c162602546182d18
timcastillogill <timgillviola@gmail.com>
2023-08-08 10:47:27 +0100

    Add div to collect basket and total

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -7,18 +7,20 @@ const Header = ({ basketTotal }) => {
         <span className="mr-0.5">octopus</span>
         <span className="antialiased font-thin">energy</span>
       </h1>
-      {basketTotal <= 1 ? null : (
-        <div className="absolute right-0">
-          <small className="flex items-end justify-center p-1 text-xs text-white rounded-full">
-            {basketTotal}
-          </small>
-        </div>
-      )}
-      <img
-        className="py-2 pr-2 fill-current h-9"
-        src="/basket.svg"
-        alt="Your Shopping Basket"
-      />
+      <div>
+        {basketTotal <= 1 ? null : (
+          <div className="absolute mb-1 right-1 md:right-7">
+            <small className="flex items-end justify-center p-1 text-xs text-white">
+              {basketTotal}
+            </small>
+          </div>
+        )}
+        <img
+          className="py-2 pr-2 fill-current h-9 md:mr-7 md:h-10"
+          src="/basket.svg"
+          alt="Your Shopping Basket"
+        />
+      </div>
     </header>
   );
 };
