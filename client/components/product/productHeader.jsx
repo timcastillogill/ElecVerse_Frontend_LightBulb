9514ebf28c3ac9dfdf9241e5b137de0619c05806
timcastillogill <timgillviola@gmail.com>
2023-08-08 10:28:56 +0100

    Add responsive styling for larger screen

--- client/components/product/productHeader.jsx
+++ client/components/product/productHeader.jsx
@@ -2,12 +2,20 @@ import React from "react";
 
 const ProductHeader = ({ product }) => {
   return (
-    <div className="m-4">
-      <img className="rounded-3xl" src={product.img_url} alt={product.name} />
-      <h1 className="mt-4 text-3xl">{product.name}</h1>
-      <h3 className="mt-1 text-sm text-slate-400">
-        {product.power} // Packet of {product.quantity}
-      </h3>
+    <div className="max-w-xl m-4 md:mx-auto md:flex md:items-center">
+      <div className="md:shrink-1">
+        <img
+          className="rounded-3xl md:h-full md:w-100"
+          src={product.img_url}
+          alt={product.name}
+        />
+      </div>
+      <div className="md:ml-10 md:w-full">
+        <h1 className="mt-4 text-3xl">{product.name}</h1>
+        <h3 className="mt-1 text-sm text-slate-400">
+          {product.power} // Packet of {product.quantity}
+        </h3>
+      </div>
     </div>
   );
 };
