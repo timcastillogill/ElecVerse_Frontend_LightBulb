bc0322d242743c29868f9a635f88cb9a172eeb6b
timcastillogill <timgillviola@gmail.com>
2023-08-08 09:14:33 +0100

    Add title to table and product.test.js now passes

---
+++ client/components/cart/cart.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Cart = ({ productName, productQuantity }) => {
+  return (
+    <section className="p-2 bg-hemocyanin">
+      <h2 className="mb-3 text-xl">Your Basket</h2>
+      <table className="text-sm text-center border-separate table-auto border-spacing-4">
+        <tbody title="Basket items">
+          <tr>
+            <th>Product</th>
+            <th>Quantity</th>
+          </tr>
+          <tr>
+            <td>{productName}</td>
+            <td>{productQuantity}</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+  );
+};
+
+export default Cart;
