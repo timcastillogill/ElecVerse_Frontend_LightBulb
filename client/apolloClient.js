654b5d5e39a0e2953f7f1750b8a33b2bc78f6d63
timcastillogill <timgillviola@gmail.com>
2023-08-09 09:51:19 +0100

    update uri for environment variable

--- client/apolloClient.js
+++ client/apolloClient.js
@@ -3,7 +3,7 @@ import fetch from "cross-fetch";
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: "http://localhost:3001/graphql",
+    uri: process.env.DB_HOST,
     fetch,
   }),
   cache: new InMemoryCache(),
