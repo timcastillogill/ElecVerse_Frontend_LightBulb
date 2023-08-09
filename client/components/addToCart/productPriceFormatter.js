fe188b4f529f90ca093f96d7f69cd040ac14b1bc
timcastillogill <timgillviola@gmail.com>
2023-08-09 10:43:15 +0100

    Remove type check in this function

--- client/components/addToCart/productPriceFormatter.js
+++ client/components/addToCart/productPriceFormatter.js
@@ -1,13 +1,10 @@
 export const productPriceFormatter = (productPrice) => {
-  if (typeof productPrice === "number") {
-    const productPriceToString = productPrice.toString();
+  const productPriceToString = productPrice.toString();
 
-    if (productPriceToString.length < 3) {
-      return `0.${productPriceToString}`;
-    }
-    const subStringBeforeLastTwoCharacters = productPriceToString.slice(0, -2);
-    const lastTwoCharacters = productPriceToString.slice(-2);
-    return `£${subStringBeforeLastTwoCharacters}.${lastTwoCharacters}`;
+  if (productPriceToString.length < 3) {
+    return `0.${productPriceToString}`;
   }
-  return "N/A";
+  const subStringBeforeLastTwoCharacters = productPriceToString.slice(0, -2);
+  const lastTwoCharacters = productPriceToString.slice(-2);
+  return `£${subStringBeforeLastTwoCharacters}.${lastTwoCharacters}`;
 };
